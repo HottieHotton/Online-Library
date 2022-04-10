27795e5746dee07a9e7ce3c6bfa28b62dc1b2224
Bhotton25 <bhotton25@gmail.com>
2022-04-10 14:06:20 -0600

    Added the rest of the GraphQL data, will work on front end

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Book{
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
+
+type User{
+ _id: ID
+ username: String!
+ email: String
+ bookCount: Int16Array
+ savedBooks: [Book]
+}
+
+type Query{
+    me: User
+}
+
+type Auth{
+pass: ID!
+user: User
+}
+
+input bookInput {
+authors: [String]
+description: String
+bookId: String
+image: String
+link: String
+title: String
+}
+
+type Mutation{
+login(email: String!, password: String!): Auth
+addUser(username: String!, email: String!, password: String!): Auth
+saveBook(bookData : bookInput!): User
+removeBook(bookId : ID!): User
+}
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
+//Apollo Calls
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs , resolvers } = require('./schema');
+const { authMiddleware } = require('./utils/auth');
+
+const startUp = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log('Go to https://localhost:${PORT}${server.graphqlPath} to view the backend information!')
+}
+
+startUp();
+
+//Express
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
